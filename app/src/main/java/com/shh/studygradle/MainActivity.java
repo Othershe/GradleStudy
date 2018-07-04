17f9e1152369e9a05e1b1fb382506dec3d3c1421
Oscar <shehuan320@163.com>
2018-07-04 18:30:44 +0800

    测试 dimension

--- app/src/main/java/com/shh/studygradle/MainActivity.java
+++ app/src/main/java/com/shh/studygradle/MainActivity.java
@@ -2,6 +2,8 @@ package com.shh.studygradle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv_tip);
+        tv.setText(BuildConfig.TIP);
     }
 }
